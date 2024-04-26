471626c64294564a57adfd38830b7af4c3989c2e
farzad_nari <farzadnari10@gmail.com>
2024-04-26 18:22:14 +0330

    define post method in login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from . import forms
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 
 class Login(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = LoginForm()
+        return render(request, 'accounts/login.html', context={'form':form})
+    
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data()
+            user = authenticate(username=cd['phone'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                form.add_error('phone', 'invalid user data')
+        else:
+            form.add_error('phone', 'invalid data')
+            
         return render(request, 'accounts/login.html', context={'form':form})
