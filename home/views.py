91f29bed288f751fb5c082d8b0f461dfc62e7d09
farzad_nari <farzadnari10@gmail.com>
2024-07-31 00:36:20 +0330

    Home templateview  edited for session test

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -5,5 +6,12 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        self.request.session['name'] = 'farzad'
+        print(self.request.session['name'])
+        return context
         
     
