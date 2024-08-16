c842a975762bbef6c84b7266458385d7e8fc29e3
farzad_nari <farzadnari10@gmail.com>
2024-08-16 20:26:05 +0330

    import and use Cart class in cart.views

--- cart/views.py
+++ cart/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from products.models import Product
+from .cart_module import Cart
 
 
 class CartDetail(View):
     def get(self, request):
-        return render(request, 'cart/cart_detail.html')
+        cart = Cart(request)
+        return render(request, 'cart/cart_detail.html', context={'cart':cart})
     
 
 class CartAdd(View):
     def post(self, request, pk):
         product = get_object_or_404(Product, pk)
         size, color, quantity = request.POST.get('size'), request.POST.get('color'), request.POST.get('quantity')
-        print('product added')
+        cart = Cart(request)
+        cart.add(product, color, size, quantity)
         return redirect('cart:cart_detail')
