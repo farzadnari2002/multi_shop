8151cedacef810be12235732f60522fde490117b
farzad_nari <farzadnari10@gmail.com>
2024-08-20 01:02:17 +0330

    stating settings remove form product urls.py and add in multi_shop settings

--- multi_shop/urls.py
+++ multi_shop/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +27,4 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from .views import *
-from django.conf.urls.static import static
-from multi_shop import settings
 
 
 app_name = 'products'
 urlpatterns = [
     path('<int:pk>', ProductDetail.as_view(), name='product_detail')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
 
