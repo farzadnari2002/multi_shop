e5693e8c4961b29a9b9f3272822547b2deedef19
farzad_nari <farzadnari10@gmail.com>
2024-09-10 17:09:22 +0330

    Category model created

--- products/admin.py
+++ products/admin.py
@@ -10,6 +10,12 @@ class InformationAdmin(admin.StackedInline):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price')
     inlines = (InformationAdmin,)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'parent')
+    prepopulated_fields = {'slug':('title',)}
     
     
 admin.site.register(Size)
--- products/models.py
+++ products/models.py
@@ -35,3 +35,13 @@ class Information(models.Model):
 
     def __str__(self):
         return self.text[:30]
+    
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE, related_name='subs')
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+
+    def __str__(self):
+        return self.title
