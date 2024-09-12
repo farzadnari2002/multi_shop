bf3f007102cad9fdc157ac99371641044d3abe40
farzad_nari <farzadnari10@gmail.com>
2024-09-12 14:48:49 +0330

    use product list template filters for context send

--- products/views.py
+++ products/views.py
@@ -21,6 +21,25 @@ class ProductsList(ListView):
     template_name = 'products/products_list.html'
     queryset = Product.objects.all()
     
+    def get_context_data(self, **kwargs):
+        request = self.request
+        colors = request.GET.getlist('color')
+        sizes = request.GET.getlist('size')
+        min_price = request.GET.get('min_price')
+        max_price = request.GET.get('max_price')
+        queryset = Product.objects.all()
+        for product in queryset:
+            print(product.price)
+        if sizes:
+            queryset = queryset.filter(color__title__in=colors).distinct()
+        if colors:
+            queryset = queryset.filter(color__title__in=colors).distinct()
+        if min_price and max_price:
+            queryset = queryset.filter(price__gte=min_price, price__lte=max_price)       
+        context = super(ProductsList, self).get_context_data()
+        context['object_list'] = queryset
+        return context
+    
 
 
 
